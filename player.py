b0401773ba6796bffd5a73eedde32ce11b77ae6e
9tailsfox2 <alexsoria1994@yahoo.com>
2025-05-19 19:38:56 -0400

    We can rotate now

--- player.py
+++ player.py
@@ -1,6 +1,5 @@
 from circleshape import *
 from constants import *
-from main import *
 class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
@@ -19,9 +18,16 @@ class Player(CircleShape):
 	def rotate(self, dt):
 		self.rotation += (PLAYER_TURN_SPEED * dt)
 	def update(self, dt):
-        	keys = pygame.key.get_pressed()
+		keys = pygame.key.get_pressed()
 
-        	if keys[pygame.K_a]:
+		if keys[pygame.K_a]:
             		self.rotate(-dt)
-        	if keys[pygame.K_d]:
+		if keys[pygame.K_d]:
             		self.rotate(dt)
+		if keys[pygame.K_w]:
+			self.move(dt)
+		if keys[pygame.K_d]:
+			self.move(dt)
+	def move(self, dt):
+		forward = pygame.Vector2(0, 1).rotate(self.rotation)
+		self.position += forward * PLAYER_SPEED * dt
