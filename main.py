e0b3012dba9991a14fee25096476c799c315dfe5
9tailsfox2 <alexsoria1994@yahoo.com>
2025-05-23 18:12:54 -0400

    we got bullets!

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ from player import *
 from circleshape import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
+all_shots = pygame.sprite.Group()
 Player.containers = (updatable, drawable)
 Asteroid.containers = (asteroids, updatable, drawable)
 AsteroidField.containers = (updatable)
+Shot.containers = (updatable, drawable, all_shots)
 AsteroidField = AsteroidField()
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 1/60
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.DOUBLEBUF)
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, all_shots)
 	print ("Starting Asteroids!")
 	print (f"Screen width: {SCREEN_WIDTH}")
 	print (f"Screen height: {SCREEN_HEIGHT}")
--- player.py
+++ player.py
@@ -1,22 +1,24 @@
 import math
 from circleshape import *
 from constants import *
+from shot import *
 class Player(CircleShape):
-	def __init__(self, x, y):
+	def __init__(self, x, y, all_shots):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.radius = PLAYER_RADIUS
 		self.rotation = 0
 		width = height = int(math.ceil(PLAYER_RADIUS * math.sqrt(2) * 2))
 		self.image = pygame.Surface((width, height), pygame.SRCALPHA)
 		self.rect = self.image.get_rect(center=(x, y))
+		self.all_shots = all_shots
 	# in the player class
 	def triangle(self):
-    		forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    		right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    		a = self.position + forward * self.radius
-    		b = self.position - forward * self.radius - right
-    		c = self.position - forward * self.radius + right
-    		return [a, b, c]
+		forward = pygame.Vector2(0, 1).rotate(self.rotation)
+		right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+		a = self.position + forward * self.radius
+		b = self.position - forward * self.radius - right
+		c = self.position - forward * self.radius + right
+		return [a, b, c]
 	def draw(self, screen):
 		self.image.fill((0, 0, 0, 0))
 		local_points = [point - self.rect.center + pygame.Vector2(self.rect.width // 2, self.rect.height // 2) for point in self.triangle()]
@@ -26,16 +28,26 @@ class Player(CircleShape):
 	def rotate(self, dt):
 		self.rotation += (PLAYER_TURN_SPEED * dt)
 	def update(self, dt):
+		self.position += (self.velocity * dt)
 		keys = pygame.key.get_pressed()
 		self.rect.center = self.position
 		if keys[pygame.K_a]:
-            		self.rotate(-dt)
+			self.rotate(-dt)
 		if keys[pygame.K_d]:
-            		self.rotate(dt)
+			self.rotate(dt)
 		if keys[pygame.K_w]:
 			self.move(dt)
 		if keys[pygame.K_s]:
 			self.move(-dt)
+		if keys[pygame.K_SPACE]:
+			self.shoot(dt)
 	def move(self, dt):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
 		self.position += forward * PLAYER_SPEED * dt
+	def shoot(self, dt):
+		new_shot = Shot(self.position.x, self.position.y)
+		velocity = pygame.Vector2(0, 1)
+		velocity = velocity.rotate(self.rotation)
+		velocity *= PLAYER_SHOOT_SPEED
+		new_shot.velocity = velocity
+		self.all_shots.add(new_shot)
